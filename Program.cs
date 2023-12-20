761ee9e00e899c0227c2f3d1138f216f702e5918
Lynx2209 <csi23-jconazo@colegioaltair.net>
2023-12-20 10:49:11 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ejercicio5arrays
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+
+            Random random = new Random();
+            Console.WriteLine("Eliga el tamaño que quieres que tenga el vector :");
+            int tamanioVector = Convert.ToInt32(Console.ReadLine());
+
+            int[] Vnumeros = new int[tamanioVector];
+
+            for (int i = 0; i < tamanioVector; i++)
+            {
+                int valorRandom = random.Next(1, 100);
+                Vnumeros[i] = valorRandom;
+
+            }
+            Array.Sort(Vnumeros);
+            for (int k = 0; k < tamanioVector; k++)
+            {
+                Console.WriteLine(Vnumeros[k]);
+            }
+
+        }
+    }
+}
